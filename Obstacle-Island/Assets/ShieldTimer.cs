15d96fd6d89dfb6f75b16929456e1aae49703dfc
Audrey-L. St-Louis <audreylaurestlouis@gmail.com>
2019-04-17 11:24:25 -0400

    Added Shield Timer

---
+++ Obstacle-Island/Assets/ShieldTimer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ShieldTimer : MonoBehaviour
+{
+    Image fillImg;
+    float timeAmt = 10;
+    float time;
+    private bool isTicking;
+
+    // Use this for initialization
+    void Start()
+    {
+        isTicking = false;
+        fillImg = this.GetComponent<Image>();
+        time = timeAmt;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (time > 0)
+        {
+            time -= Time.deltaTime;
+            fillImg.fillAmount = time / timeAmt;
+            isTicking = true;
+        }
+        else
+            isTicking = false;
+    }
+
+
+    void playTimer()
+    {
+        FixedUpdate();
+    }
+}
+
+
+// Code reuse from http://aarlangdi.blogspot.com/2016/02/making-health-bar-in-unity-5-radial.html
+// Added isTicking variable to keep track of the Timer being on.
